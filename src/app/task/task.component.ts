3d0b0563235934ed1995ec3c1a68ff2c1ca25fd1
Marnel Fresh'eur <gmlginolias@gmail.com>
2021-01-21 00:26:28 +0100

    Adding @angular/material, taskComponent and a basic interface in the app.component

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Task} from './task';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() task: Task;
+  @Output() edit: EventEmitter<Task>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
